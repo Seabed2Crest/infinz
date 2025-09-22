3540534ddf21cfca4829c640f310610971298fa1
abhiraj-a-r-dev <abhiraj@seabed2crest.com>
2025-09-22 18:52:47 +0530

    removed eslint

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\s2c\infinz\app\page.tsx
+// File: C:\seabed\infinz\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
