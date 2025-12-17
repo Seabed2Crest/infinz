5d81c0e885b72cd9130b344990086e784f8b55b9
abhiraj-a-r-dev <abhiraj@seabed2crest.com>
2025-12-18 05:24:16 +0530

    lender chnages

--- app/components/OurLenders.tsx
+++ app/components/OurLenders.tsx
@@ -23,13 +23,7 @@ const lenders = [
 
 const colorClasses = {
   blue: "from-blue-500 to-blue-600",
-  orange: "from-orange-500 to-orange-600",
-  red: "from-red-500 to-red-600",
   green: "from-green-500 to-green-600",
-  purple: "from-purple-500 to-purple-600",
-  indigo: "from-indigo-500 to-indigo-600",
-  pink: "from-pink-500 to-pink-600",
-  yellow: "from-yellow-500 to-yellow-600",
 };
 
 function LenderCard({ lender, index }: { lender: any; index: number }) {
